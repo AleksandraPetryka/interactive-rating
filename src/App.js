1f11e2f803d0679dc697e922f7cc2ce623baf898
Aleksandra Petryka <aleksandra.petryka@gmail.com>
2022-04-22 11:18:26 +0200

    add basic html

--- src/App.js
+++ src/App.js
@@ -2,7 +2,22 @@ import './App.css';
 
 function App() {
   return (
-    <div></div>
+    <section>
+      <h1>How did we do?</h1>
+      <p>Please let us know how we did with your support request. All feedback is appreciated
+        to help us improve our offering!
+      </p>
+      <div>
+        <button className="rating-btn">1</button>
+        <button className="rating-btn">2</button>
+        <button className="rating-btn">3</button>
+        <button className="rating-btn">4</button>
+        <button className="rating-btn">5</button>
+      </div>
+      <div>
+        <button>Submit</button>
+      </div>
+    </section>
   );
 }
 
