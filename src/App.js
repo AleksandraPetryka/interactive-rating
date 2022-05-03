0984f7be914ce8f2151606c1d6b9e880b0d27b38
Aleksandra Petryka <aleksandra.petryka@gmail.com>
2022-05-03 12:13:56 +0200

    add logic

--- src/App.js
+++ src/App.js
@@ -2,7 +2,20 @@ import './App.css';
 import Card from './components/Card';
 import IconStar from './assets/icon-star.svg';
 
+let RATING_SCORE = {};
+
 function App() {
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log('You clicked submit with score: ', RATING_SCORE);
+  }
+
+  function rateHandler(score) {
+    RATING_SCORE = {score};
+    console.log('Rate has been hanged to: ', RATING_SCORE);
+  }
+
   return (
     <Card>
       <button className="rating-btn"><img src={IconStar} alt="star"></img></button>
@@ -11,14 +24,14 @@ function App() {
         to help us improve our offering!
       </p>
       <div>
-        <button className="rating-btn">1</button>
-        <button className="rating-btn">2</button>
-        <button className="rating-btn">3</button>
-        <button className="rating-btn">4</button>
-        <button className="rating-btn">5</button>
+        <button className="rating-btn" onClick={(() => rateHandler(1))}>1</button>
+        <button className="rating-btn" onClick={(() => rateHandler(2))}>2</button>
+        <button className="rating-btn" onClick={(() => rateHandler(3))}>3</button>
+        <button className="rating-btn" onClick={(() => rateHandler(4))}>4</button>
+        <button className="rating-btn" onClick={(() => rateHandler(5))}>5</button>
       </div>
       <div>
-        <button className="submit-btn">SUBMIT</button>
+        <button className="submit-btn" onClick={handleSubmit}>SUBMIT</button>
       </div>
     </Card>
   );
