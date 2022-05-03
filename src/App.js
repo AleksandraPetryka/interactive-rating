d0efdf7ed72b5c18f98fec046a2bb0c79b1e3734
Aleksandra Petryka <aleksandra.petryka@gmail.com>
2022-05-03 10:25:48 +0200

    star button

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import './App.css';
+import Card from './components/Card';
+import IconStar from './assets/icon-star.svg';
 
 function App() {
   return (
-    <section>
+    <Card>
+      <button className="rating-btn"><img src={IconStar} alt="star"></img></button>
       <h1>How did we do?</h1>
       <p>Please let us know how we did with your support request. All feedback is appreciated
         to help us improve our offering!
@@ -15,9 +18,9 @@ function App() {
         <button className="rating-btn">5</button>
       </div>
       <div>
-        <button>Submit</button>
+        <button className="submit-btn">SUBMIT</button>
       </div>
-    </section>
+    </Card>
   );
 }
 
